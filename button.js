fd182e2c99423e9a5f6236543a682b05bb7ba3df
natsuki282 <natsuki.kb.282@gmail.com>
2022-06-15 20:04:09 +0900

    ボタンテスト(失敗)

---
+++ button.js
@@ -0,0 +1,6 @@
+document.querySelector("#t5").addEventListener('click', function() {
+    console.log("t5");
+});
+document.querySelector("#d19").addEventListener('click', function() {
+    console.log("d19");
+});
